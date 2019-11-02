7901233aeccdc8570c59d5c0bebcbe888a0fd73d
Maggie Greene <maggie.greene96@gmail.com>
2019-11-02 12:33:34 -0500

    Updated smash function

--- src/javascripts/helpers/data/smash.js
+++ src/javascripts/helpers/data/smash.js
@@ -1,10 +1,14 @@
 import machineData from './machineData';
 import positionData from './positionData';
+import snackPositionData from './snackPositionData';
 
 const getCompleteMachine = () => new Promise((resolve, reject) => {
   machineData.getMachine()
     .then((singleMachine) => positionData.getAllPositionsByMachineId(singleMachine.id))
-    .then((positions) => resolve(positions))
+    .then((positions) => {
+      snackPositionData.getAllSnackPositionsByMachineId(positions[0].machineId)
+        .then((snackPositions) => resolve(snackPositions));
+    })
     .catch((error) => reject(error));
 });
 
