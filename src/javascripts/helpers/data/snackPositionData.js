5949846fae33cfeae23527a4ed8306dca09d3d54
Maggie Greene <maggie.greene96@gmail.com>
2019-11-04 21:16:44 -0600

    Can remove and add objects from machine

--- src/javascripts/helpers/data/snackPositionData.js
+++ src/javascripts/helpers/data/snackPositionData.js
@@ -18,5 +18,6 @@ const getAllSnackPositionsByMachineId = (machineId) => new Promise((resolve, rej
 });
 
 const deleteSnackPosition = (snackPositionId) => axios.delete(`${baseUrl}/snackPositions/${snackPositionId}.json`);
+const createSnackPosition = (newSnackPosition) => axios.post(`${baseUrl}/snackPositions.json`, newSnackPosition);
 
-export default { getAllSnackPositionsByMachineId, deleteSnackPosition };
+export default { getAllSnackPositionsByMachineId, deleteSnackPosition, createSnackPosition };
