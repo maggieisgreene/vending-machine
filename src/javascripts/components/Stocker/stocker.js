fe552affa99b615f6f3b262a4c322cff9302859c
Maggie Greene <maggie.greene96@gmail.com>
2019-11-05 18:52:50 -0600

    Added new snack to stocker

--- src/javascripts/components/Stocker/stocker.js
+++ src/javascripts/components/Stocker/stocker.js
@@ -5,6 +5,7 @@ import './stocker.scss';
 import smash from '../../helpers/data/smash';
 import utilities from '../../helpers/utilities';
 import stockCard from '../StockCard/stockCard';
+import snackData from '../../helpers/data/snackData';
 import snackPositionData from '../../helpers/data/snackPositionData';
 import machine from '../Machine/machine';
 
@@ -46,10 +47,35 @@ const addToMachine = (event) => {
     .catch((error) => console.error(error));
 };
 
+const addNewSnack = (event) => {
+  event.stopImmediatePropagation();
+  const { uid } = firebase.auth().currentUser;
+  const newSnack = {
+    imageUrl: $('#snack-image-url').val(),
+    name: $('#snack-name').val(),
+    price: $('#snack-price').val() * 1,
+    currentStocked: 0,
+    lifetimeNum: 0,
+    uid,
+  };
+  snackData.addNewSnack(newSnack)
+    .then(() => {
+      $('#exampleModal').modal('hide');
+      // eslint-disable-next-line no-use-before-define
+      buildTheStocker(uid);
+    })
+    .catch((error) => console.error(error));
+};
+
 const buildTheStocker = (uid) => {
   smash.getSnacksWithPositions(uid)
     .then((snacks) => {
       let domString = '<h2>STOCK THE MACHINE</h2>';
+      domString += `
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+        Add Snack
+      </button>
+      `;
       domString += '<div class="d-flex flex-wrap">';
       snacks.forEach((snack) => {
         domString += stockCard.makeASnack(snack);
@@ -58,6 +84,7 @@ const buildTheStocker = (uid) => {
       utilities.printToDom('stock', domString);
       $('#stock').on('click', '.delete-snack-position', deleteFromMachine);
       $('#stock').on('click', '.add-snack-position', addToMachine);
+      $('#add-new-snack').click(addNewSnack);
     })
     .catch((error) => console.error(error));
 };
