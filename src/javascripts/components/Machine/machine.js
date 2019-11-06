96ced89bac38ec255a6081def0e9b0287239f348
Maggie Greene <maggie.greene96@gmail.com>
2019-11-05 20:41:08 -0600

    Buy stock func works and button disables when zero is available

--- src/javascripts/components/Machine/machine.js
+++ src/javascripts/components/Machine/machine.js
@@ -1,7 +1,18 @@
 import './machine.scss';
+import $ from 'jquery';
 import smash from '../../helpers/data/smash';
 import snack from '../Snacks/snacks';
 import utilities from '../../helpers/utilities';
+import snackData from '../../helpers/data/snackData';
+
+const buySnack = (event) => {
+  event.stopImmediatePropagation();
+  const snackId = event.target.id.split('buy-')[1];
+  snackData.buySnack(snackId)
+    // eslint-disable-next-line no-use-before-define
+    .then(() => buildTheMachine())
+    .catch((error) => console.error(error));
+};
 
 const buildTheMachine = () => {
   smash.getCompleteMachine()
@@ -13,6 +24,7 @@ const buildTheMachine = () => {
       });
       domString += '</div>';
       utilities.printToDom('machine', domString);
+      $('#machine').on('click', '.buy-snack', buySnack);
     })
     .catch((error) => console.error(error));
 };
