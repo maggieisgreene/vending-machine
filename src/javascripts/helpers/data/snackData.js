eec126e80abc970ba7c4bf2118b49fe3a4bc7de8
Maggie Greene <maggie.greene96@gmail.com>
2019-11-05 19:27:08 -0600

    Stock numbers increase with button

--- src/javascripts/helpers/data/snackData.js
+++ src/javascripts/helpers/data/snackData.js
@@ -18,5 +18,18 @@ const getSnacksByUid = (uid) => new Promise((resolve, reject) => {
 });
 
 const addNewSnack = (newSnack) => axios.post(`${baseUrl}/snacks.json`, newSnack);
+const updateSnack = (snackId, updatedSnack) => axios.put(`${baseUrl}/snacks/${snackId}.json`, updatedSnack);
 
-export default { getSnacksByUid, addNewSnack };
+const restock = (snackId, restockNum) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/snacks/${snackId}.json`)
+    .then((result) => {
+      const snackObject = result.data;
+      snackObject.currentStocked += restockNum;
+      snackObject.lifetimeNum += restockNum;
+      updateSnack(snackId, snackObject);
+      resolve();
+    })
+    .catch((error) => reject(error));
+});
+
+export default { getSnacksByUid, addNewSnack, restock };
